372131043ad85c889238417baf5e5696833233f3
Basil Salahat <112906018+basilsalahat@users.noreply.github.com>
2023-12-06 14:16:20 +0200

    Filter (Return a result for only one filter option) (#4)
    * Handle the search bar
    
    * Handle the search bar and how
    the cards looks after search
    
    * add isEmpty func to card component
    
    * Handle filter (result for one filter option only)
    
    * Fix filter selection
    
    * Replace filter select option ro radio buttons

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -4,22 +4,91 @@ import SearchBar from "./SearchBar";
 import { useState } from "react";
 import Card from "./Card";
 import { StyledContent } from "./Content.styled";
+import Filters from "./Filters";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import Button from "@mui/material/Button";
 
 export default function Body() {
   const [books, setBooks] = useState([]);
+  const [filters, setFilters] = useState({});
+  const [filterdBooksList, setfilterdBooksList] = useState([]);
+  const [resultInfo, setResultInfo] = useState({});
+  const [filtersState, setFiltersState] = useState("hide");
 
-  function handleState(newValue) {
-    setBooks(newValue);
+  function handleState(newBooks, searchText, totalItemNum) {
+    setBooks(newBooks);
+    setResultInfo({
+      text: searchText.toString(),
+      num: totalItemNum.toString(),
+    });
+    setFilters({
+      authors: Object.groupBy(newBooks, ({ volumeInfo }) =>
+        volumeInfo.authors ? volumeInfo.authors.toString().toLowerCase() : "N/A"
+      ),
+      categories: Object.groupBy(newBooks, ({ volumeInfo }) =>
+        volumeInfo.categories
+          ? volumeInfo.categories.toString().toLowerCase()
+          : "N/A"
+      ),
+      languages: Object.groupBy(newBooks, ({ volumeInfo }) =>
+        volumeInfo.language
+          ? volumeInfo.language.toString().toLowerCase()
+          : "N/A"
+      ),
+    });
+  }
+  function filteredBooks(filtersSelected) {
+    let result = [];
+    filtersSelected.forEach((filter) => {
+      for (const [filterCategory, value] of Object.entries(filters)) {
+        for (const [key] of Object.entries(value)) {
+          if (key == filter) {
+            result.push(filters[filterCategory][key]);
+          }
+        }
+      }
+    });
+    setfilterdBooksList(result);
   }
 
   return (
     <StyledBody>
       <SearchBar result={handleState} />
-      <StyledContent>
+      <StyledContent className={filtersState}>
+        {resultInfo["text"] && (
+          <div className="resultInfoContainer">
+            <h1>
+              {resultInfo["text"].toString().split("+")[0]}
+              <span>&nbsp;[{resultInfo["num"]}]</span>
+            </h1>
+            <Button
+              variant="text"
+              endIcon={<FilterAltIcon />}
+              sx={{ color: "gray", textTransform: "capitalize" }}
+              size="large"
+              onClick={() => {
+                filtersState == "show"
+                  ? setFiltersState("hide")
+                  : setFiltersState("show");
+              }}
+            >
+              {filtersState} Filter
+            </Button>
+          </div>
+        )}
+        <Filters
+          filtersData={filters}
+          updateBooks={filteredBooks}
+          filtersState={filtersState}
+        />
         <StyledCardsContainer>
-          {books.map((ele, i) => {
-            return <Card key={i} details={ele} />;
-          })}
+          {filterdBooksList.length > 0
+            ? filterdBooksList.map((ele, i) => {
+                return <Card key={i} details={ele[0]} />;
+              })
+            : books.map((ele, i) => {
+                return <Card key={i} details={ele} />;
+              })}
         </StyledCardsContainer>
       </StyledContent>
     </StyledBody>
--- src/components/body/Content.styled.jsx
+++ src/components/body/Content.styled.jsx
@@ -2,6 +2,25 @@ import styled from "styled-components";
 
 export const StyledContent = styled.div`
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: 220px 1fr;
+  grid-template-rows: 80px 1fr;
   column-gap: 12px;
+  overflow: hidden;
+  h1 {
+    text-transform: capitalize;
+    span {
+      font-size: smaller;
+      font-weight: normal;
+      color: gray;
+    }
+  }
+  .resultInfoContainer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    grid-area: span 1 / span 3;
+  }
+  &.show {
+    grid-template-columns: 1fr;
+  }
 `;
